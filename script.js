310b0ca9b1e04e2c9f4303984cfed4444aff7451
episani25 <episani25@students.stab.org>
2024-02-16 14:45:24 -0500

    2/16/24

--- script.js
+++ script.js
@@ -7,17 +7,60 @@ var artist = getColumn(url,4);
 var genre = getColumn(url,5);
 var subGenre = getColumn(url,7);
 var albumArt = getColumn(url,8);
-
+// console.log(artist)
 var yearReleased = document.getElementById("year").value;
 var preferredGenre = document.getElementById("genre").value;
-var favoriteArtist = document.getElementById("artist").value;
-var output = document.getElementById("outputBox").value;
-
-
-//First, I am going to loop through the list and look for 
 
 
 
+//First, I am going to loop through the list and look for albums put out in the year the user imputs  
+function produceAlbumFromYear(){
+    var yearReleased = document.getElementById("year").value;
+    var pickedYear = []
+    
+    for(var i = 0; i < year.length; i++){
+        
+        if(year[i].toLowerCase().includes(yearReleased.toLowerCase())){
+            pickedYear.push(album[i]);
+    
+            }
+        }
+    document.getElementById("output").innerHTML = pickedYear;
+    }
 
 
-//Next, I am going to loop through the list and search for each genre
+
+
+//Next, I am going to loop through the list and search for each genre and populate the albums in each genre
+function produceAlbumFromGenre(){
+
+    var preferredGenre = document.getElementById("genre").value;
+    var pickedGenre = [];
+    
+    for(var i = 0; i < genre.length; i++){
+        if(genre[i].toLowerCase().includes(preferredGenre.toLowerCase())){
+            pickedGenre.push(album[i]);
+    
+            }
+        }
+    console.log(pickedGenre)
+    document.getElementById("output").innerHTML = pickedGenre;
+    }
+
+
+
+//I am now going to loop through the list searching for albums created by the user imput
+function produceAlbum(){
+var favoriteArtist = document.getElementById("selectedArtist").value;
+var favArtistAlbums = []
+
+for(var i = 0; i < artist.length; i++){
+    
+    if(artist[i].toLowerCase().includes(favoriteArtist.toLowerCase())){
+        console.log(artist[i]);
+        favArtistAlbums.push(album[i]);
+
+        }
+    }
+document.getElementById("output").innerHTML = favArtistAlbums;
+}
